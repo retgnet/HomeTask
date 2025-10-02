0498f4bb19c2e5e8457d1c1100a0997cb6ef2593
Николай <maksimov13kola@gmail.com>
2025-10-02 23:27:37 +0300

    Добавили реализацию метода sumOfList, реализацию метода fillListForMethod, позволяющий нам заполнять список диапазоном значений от min до max

--- src/main/java/com/maximov/Main.java
+++ src/main/java/com/maximov/Main.java
@@ -5,12 +5,29 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(rangeBetweenMaxAndMin(-9, 25));
+        System.out.println(rangeBetweenMaxAndMin(-9, 2));
+
+        List<Integer> list = new ArrayList<>();
+        System.out.println(sumOfList(fillListForMethod(list, 0, 10)));
     }
 
-    public static List<Integer> rangeBetweenMaxAndMin(int min, int max){
+    public static List<Integer> rangeBetweenMaxAndMin(int min, int max) {
         List<Integer> list = new ArrayList<>();
-        for(int i = min; i <= max; i ++){
+        return fillListForMethod(list, min, max);
+    }
+
+    public static int sumOfList(List<Integer> list) {
+        int sum = 0;
+        for (int num : list) {
+            if (num > 5) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+
+    public static List<Integer> fillListForMethod(List<Integer> list, int minNumberOfRange, int maxNumberOfRange) {
+        for (int i = minNumberOfRange; i <= maxNumberOfRange; i++) {
             list.add(i);
         }
         return list;
