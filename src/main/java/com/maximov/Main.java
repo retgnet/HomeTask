8eab66c5ed685f02bc8d6e0c4da7ad3d2cb71e59
Николай <maksimov13kola@gmail.com>
2025-10-02 23:16:59 +0300

    Добавили реализацию метода rangeBetweenMaxAndMin и вывод в методе main

--- src/main/java/com/maximov/Main.java
+++ src/main/java/com/maximov/Main.java
@@ -1,7 +1,18 @@
 package com.maximov;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(rangeBetweenMaxAndMin(-9, 25));
+    }
 
+    public static List<Integer> rangeBetweenMaxAndMin(int min, int max){
+        List<Integer> list = new ArrayList<>();
+        for(int i = min; i <= max; i ++){
+            list.add(i);
+        }
+        return list;
     }
 }
