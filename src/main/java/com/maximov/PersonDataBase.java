62ef9a104a236bca3d72943b0086af37aa1b6927
Николай <maksimov13kola@gmail.com>
2025-10-11 15:25:08 +0300

    Сделали правку для класса PersonDataBase, асимптотическая сложность для метода isManager является O(1);

--- src/main/java/com/maximov/PersonDataBase.java
+++ src/main/java/com/maximov/PersonDataBase.java
@@ -6,6 +6,14 @@ import com.maximov.Position;
 
 public class PersonDataBase {
     private final Map<Long, Person> map = new HashMap<>();
+    private final static HashSet<Position> hashSetPosition = new HashSet<>();
+    static{
+        hashSetPosition.add(Position.MANAGER);
+        hashSetPosition.add(Position.DIRECTOR);
+        hashSetPosition.add(Position.BRANCH_DIRECTOR);
+        hashSetPosition.add(Position.SENIOR_MANAGER);
+
+    }
 
     public void add(Person person){
         map.put(person.getId(), person);
@@ -17,8 +25,7 @@ public class PersonDataBase {
         return null;
     }
     public boolean isManager(Person person){
-        return person.getPosition() == Position.MANAGER || person.getPosition() == Position.DIRECTOR || person.getPosition() == Position.BRANCH_DIRECTOR ||
-                person.getPosition() == Position.SENIOR_MANAGER;
+        return hashSetPosition.contains(person.getPosition());
     }
     public boolean isEmployee(Long id){
         return map.containsKey(id);
