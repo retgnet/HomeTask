1dd31a182a3302e72ccc645d05ffeecbe2eaf460
Николай <maksimov13kola@gmail.com>
2025-10-11 21:25:01 +0300

    Добавили возможность чтения файла

--- src/main/java/com/maximov/Main.java
+++ src/main/java/com/maximov/Main.java
@@ -1,7 +1,23 @@
 package com.maximov;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
+        try (InputStreamReader in = new InputStreamReader(new FileInputStream("demo.txt"), StandardCharsets.UTF_8)) {
+
+            int n = in.read();
+            while (n != -1) {
+                System.out.print((char) n);
+                n = in.read();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
