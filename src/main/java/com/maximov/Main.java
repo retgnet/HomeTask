94d3ad6e63f09ef8fbd6377167162369fccf505e
Николай <maksimov13kola@gmail.com>
2025-10-11 21:54:30 +0300

    Добавили реализацию и разнесли код по методам

--- src/main/java/com/maximov/Main.java
+++ src/main/java/com/maximov/Main.java
@@ -1,13 +1,48 @@
 package com.maximov;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (InputStreamReader in = new InputStreamReader(new FileInputStream("demo.txt"), StandardCharsets.UTF_8)) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Текущие файлы: ");
+
+        System.out.println("Введите имя файла, который хотите просмотреть: ");
+        checkCurrentFiles();
+        String nameFile = scanner.nextLine();
+
+        fileRead(nameFile);
+        System.out.println("Введите информацию, которую хотите записать в файл: ");
+
+        String data = scanner.nextLine();
+        writeFile(nameFile, data);
+        fileRead(nameFile);
+
+
+    }
+
+    public static void checkCurrentFiles() {
+        File directory = new File(".");
+        FilenameFilter txt = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".txt");
+            }
+        };
+        File[] txtFiles = directory.listFiles(txt);
+        if (txt != null) {
+            for (File file : txtFiles) {
+                System.out.println(file.getName());
+            }
+        } else {
+            System.out.println("Ошибка! Файлов с расширением txt не существует в данной директории! ");
+        }
+    }
+
+    public static void fileRead(String nameFile) {
+        try (InputStreamReader in = new InputStreamReader(new FileInputStream(nameFile), StandardCharsets.UTF_8)) {
 
             int n = in.read();
             while (n != -1) {
@@ -18,6 +53,17 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        System.out.println();
+    }
 
+    public static void writeFile(String nameFile, String data) {
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(nameFile))) {
+            byte[] buffer = data.getBytes(StandardCharsets.UTF_8);
+            for (int i = 0; i < buffer.length; i++) {
+                out.write(buffer[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
