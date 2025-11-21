9b2abd03d38d5376116098a3508e26e36cf28ba7
Николай <maksimov13kola@gmail.com>
2025-11-21 20:25:28 +0300

    реализация

---
+++ src/main/java/com/maximov/Apple.java
@@ -0,0 +1,8 @@
+package com.maximov;
+
+public class Apple extends Fruit {
+
+    public Apple(double weight) {
+        super(weight);
+    }
+}
--- src/main/java/com/maximov/Main.java
+++ src/main/java/com/maximov/Main.java
@@ -3,5 +3,22 @@ package com.maximov;
 public class Main {
     public static void main(String[] args) {
 
+        Box<Apple> appleBox = new Box<>();
+        Box<Orange> orangeBox = new Box<>();
+        Box<Apple> anotherAppleBox = new Box<>();
+
+        appleBox.addFruit(new Apple(0.2));
+        appleBox.addFruit(new Apple(0.25));
+        orangeBox.addFruit(new Orange(0.3));
+        orangeBox.addFruit(new Orange(0.35));
+
+        System.out.println("Вес коробки с яблоками: " + appleBox.weight());
+        System.out.println("Вес коробки с апельсинами: " + orangeBox.weight());
+
+        System.out.println("Веса коробок равны? " + appleBox.compare(orangeBox));
+
+        appleBox.pourInto(anotherAppleBox);
+        System.out.println("После пересыпания, вес второй коробки с яблоками: " + anotherAppleBox.weight());
+
     }
 }
